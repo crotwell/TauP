c40547cf712649310fe5c9eacb79a1102dc98fd1
crotwell <none@none>
2009-07-06 11:11:19 -0400

    [svn r20570] less cpu than clone, depth correct copies arrays on split, so no need to make yet another copy
    --HG--
    branch : TauP

--- src/main/java/edu/sc/seis/TauP/TauBranch.java
+++ src/main/java/edu/sc/seis/TauP/TauBranch.java
@@ -77,6 +77,26 @@ public class TauBranch implements Serializable, Cloneable {
     protected double[] tau;
 
     // Constructors --------------------------------------------------------
+    public TauBranch(boolean isPWave,
+                     double topDepth,
+                     double botDepth,
+                     double maxRayParam,
+                     double minTurnRayParam,
+                     double minRayParam,
+                     double[] dist,
+                     double[] time,
+                     double[] tau) {
+        this.isPWave = isPWave;
+        this.topDepth = topDepth;
+        this.botDepth = botDepth;
+        this.maxRayParam = maxRayParam;
+        this.minTurnRayParam = minTurnRayParam;
+        this.minRayParam = minRayParam;
+        this.dist = dist;
+        this.time = time;
+        this.tau = tau;
+    }
+    
     public TauBranch(double topDepth, double botDepth, boolean isPWave) {
         this.topDepth = topDepth;
         this.botDepth = botDepth;
@@ -721,20 +741,20 @@ public class TauBranch implements Serializable, Cloneable {
      * 
      * @see Cloneable
      */
-    public Object clone() {
-        TauBranch newObject;
-        try {
-            newObject = (TauBranch)super.clone();
-            newObject.dist = (double[])dist.clone();
-            newObject.time = (double[])time.clone();
-            newObject.tau = (double[])tau.clone();
-            return newObject;
-        } catch(CloneNotSupportedException e) {
-            // Can't happen, but...
-            System.err.println("Caught CloneNotSupportedException: "
-                    + e.getMessage());
-            throw new InternalError(e.toString());
-        }
+    public TauBranch clone() {
+//        double[] newDist = new double[dist.length];
+//        System.arraycopy(dist, 0, newDist, 0, dist.length);
+//        double[] newTime = new double[time.length];
+//        System.arraycopy(time, 0, newTime, 0, dist.length);
+//        double[] newTau = new double[tau.length];
+//        System.arraycopy(tau, 0, newTau, 0, dist.length);
+        return new TauBranch(isPWave,
+                             topDepth,
+                             botDepth,
+                             maxRayParam,
+                             minTurnRayParam,
+                             minRayParam,
+                             dist, time, tau);
     }
 
     public String toString() {
