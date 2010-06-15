4ac876e60927e423559ff976715d206a4fcff74d
crotwell <none@none>
2010-06-15 13:58:55 -0400

    [svn r21390] add SeismicPhase, loadTauModel, ellip and spherDist
    --HG--
    branch : TauP

--- src/main/resources/edu/sc/seis/TauP/jython/taup/__init__.py
+++ src/main/resources/edu/sc/seis/TauP/jython/taup/__init__.py
@@ -4,13 +4,27 @@ from edu.sc.seis.TauP import TauP_Path
 from edu.sc.seis.TauP import TauP_Pierce
 from edu.sc.seis.TauP import TauP_Curve
 from edu.sc.seis.TauP import SphericalCoords
+from edu.sc.seis.TauP import TauModelLoader
+from edu.sc.seis.TauP import SeismicPhase
+from java.lang import System
+import distaz
 
-
-__all__ = ['taup', 'dist']
+__all__ = ['taup', 'spherDist', 'ellipDist', 'loadTauModel', 'SeismicPhase']
 
 
 taup = TauP_Time('iasp91')
 
-def dist(stla, stlo, evla, evlo):
+def spherDist(stla, stlo, evla, evlo):
+    """spherical distance in degrees"""
     return SphericalCoords.distance(stla, stlo, evla, evlo)
+
+def ellipDist(stla, stlo, evla, evlo):
+    """elliptical distance in degrees"""
+    d = distaz.DistAz(stla, stlo, evla, evlo)
+    return d.getDelta()
+
+def loadTauModel(name, path=System.getProperties().getProperty("taup.model.path"), verbose=False):
+    return TauModelLoader.load(name, path, verbose)
+
+ 
 
