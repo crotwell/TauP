4b290e7894a0b62a0d310040ca90775ebadabe90
crotwell <none@none>
2010-10-22 15:45:17 -0400

    [svn r21787] initial
    --HG--
    branch : TauP

---
+++ src/test/java/edu/sc/seis/TauP/SurfaceWaveTest.java
@@ -0,0 +1,43 @@
+package edu.sc.seis.TauP;
+
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SurfaceWaveTest  extends TestCase {
+
+    @Before
+    @Test
+    protected void setUp() throws Exception {
+        taup = new TauP_Time("ak135");
+    }
+    
+    TauP_Time taup;
+
+    
+    @Test
+    public void testSurfaceWave() throws TauModelException {
+        String phaseName = "3.0kmps";
+        float depth = 23;
+        float deg = 30;
+            taup.setPhaseNames(new String[] {phaseName});
+            taup.depthCorrect(depth);
+            taup.calculate(deg);
+            List<Arrival> arrivals = taup.getArrivals();
+            assertTrue(phaseName + " has arrivals for depth " + depth + " at dist " + deg,
+                       arrivals.size() > 0);
+            assertEquals(phaseName + " time for depth " + depth + " at dist " + deg,
+                         1111.95f,
+                         arrivals.get(0).time,
+                         0.07f);
+            // long way around
+            assertEquals(phaseName + " time for depth " + depth + " at dist " + deg,
+                         12231.44f,
+                         arrivals.get(1).time,
+                         0.07f);
+    }
+}
