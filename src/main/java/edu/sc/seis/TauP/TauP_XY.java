af90f6a659fc329ffef4f5625dcf45d88fb93c18
Philip Crotwell <crotwell@seis.sc.edu>
2024-03-14 14:53:47 -0400

    reducing vel, add kilometer distance option?

--- src/main/java/edu/sc/seis/TauP/TauP_XY.java
+++ src/main/java/edu/sc/seis/TauP/TauP_XY.java
@@ -47,17 +47,17 @@ public class TauP_XY extends TauP_AbstractTimeTool {
                 if (dashEquals("x", arg)) {
                     xAxisType = args[j+1];
                     j++;
-                } else if (dashEquals("xlog", arg)) {
-                    noComprendoArgs.remove(arg);
-                    yAxisType = args[j+1];
-                    j++;
                 } else if (dashEquals("y", arg)) {
                     noComprendoArgs.remove(arg);
                     yAxisType = args[j+1];
                     j++;
-                } else if (dashEquals("ylog", arg)) {
-                    noComprendoArgs.remove(arg);
-                    yAxisType = args[j+1];
+                } else if(dashEquals("reddeg", arg)) {
+                    setReduceTime(true);
+                    setReduceVelDeg(Double.parseDouble(args[j + 1]));
+                    j++;
+                } else if(dashEquals("redkm", arg)) {
+                    setReduceTime(true);
+                    setReduceVelKm(Double.parseDouble(args[j + 1]));
                     j++;
                 } else if (j < args.length-2) {
                     if (dashEquals("xminmax", arg)) {
@@ -120,8 +120,11 @@ public class TauP_XY extends TauP_AbstractTimeTool {
         }
     }
 
-    public Map<SeismicPhase, List<XYPlottingData>> calculate(String xAxisType, String yAxisType) throws TauModelException, VelocityModelException, SlownessModelException {
+    public Map<SeismicPhase, List<XYPlottingData>> calculate(String xAxisType, String yAxisType) throws TauPException {
         Map<SeismicPhase, List<XYPlottingData>>  xy = calculateLinear(xAxisType, yAxisType);
+        if (isReduceTime()) {
+            xy = reduce(xy);
+        }
         if (xAxisLog || yAxisLog) {
             for (SeismicPhase phase : xy.keySet()) {
                 xy.put(phase, recalcForLog(xy.get(phase), xAxisLog, yAxisLog));
@@ -192,11 +195,56 @@ public class TauP_XY extends TauP_AbstractTimeTool {
         return outMap;
     }
 
+    public Map<SeismicPhase, List<XYPlottingData>> reduce(Map<SeismicPhase, List<XYPlottingData>> xy) {
+        Map<SeismicPhase, List<XYPlottingData>> out = new HashMap<>();
+        double velFactor;
+        if (axisIsDistance(xAxisType) && axisIsTime(yAxisType)) {
+            // x is distance, y is time
+            velFactor = reduceVelForAxis(xAxisType);
+        } else if (axisIsDistance(yAxisType) && axisIsTime(xAxisType)) {
+            velFactor = reduceVelForAxis(yAxisType);
+        } else {
+            throw new IllegalArgumentException("axis are not time and distance: "+xAxisType+" "+yAxisType);
+        }
+
+        for (SeismicPhase phase : xy.keySet()) {
+            List<XYPlottingData> plotList = xy.get(phase);
+            List<XYPlottingData> redplotList = new ArrayList<>();
+            for (XYPlottingData xyp : plotList) {
+                List<XYSegment> redSeg = new ArrayList<>();
+                for (XYSegment xyseg : xyp.segmentList) {
+                    double[] dist;
+                    double[] time;
+                    if (axisIsDistance(xAxisType)) {
+                        dist = xyseg.x;
+                        time = xyseg.y;
+                    } else {
+                        dist = xyseg.y;
+                        time = xyseg.x;
+                    }
+                    for (int i = 0; i < dist.length; i++) {
+                        time[i] = time[i] - dist[i] / velFactor;
+                    }
+                    if (axisIsDistance(xAxisType)) {
+                        redSeg.add(new XYSegment(dist, time));
+                    } else {
+                        redSeg.add(new XYSegment(time, dist));
+                    }
+                }
+                XYPlottingData redxyp = new XYPlottingData(redSeg, xyp.xAxisType, xyp.yAxisType, xyp.label, xyp.phase);
+                redplotList.add(redxyp);
+            }
+            out.put(phase, redplotList);
+        }
+        return out;
+    }
+
     public static final String[] axisTypes = new String[] {
             "radian",
             "radian_pi",
             "degree",
             "degree_180",
+            "kilometer",
             "rayparam",
             "time",
             "tau",
@@ -218,6 +266,12 @@ public class TauP_XY extends TauP_AbstractTimeTool {
             out = phase.getDist();
             for (int i = 0; i < out.length; i++) {
                 out[i] *= 180/Math.PI;
+            }
+        } else if (axisType.equalsIgnoreCase("kilometer") || axisType.equalsIgnoreCase("kilometer_180")) {
+            out = phase.getDist();
+            double redToKm = tMod.getRadiusOfEarth();
+            for (int i = 0; i < out.length; i++) {
+                out[i] *= redToKm;
             }
         } else if (axisType.equalsIgnoreCase("rayparam")) {
             out = phase.getRayParams();
@@ -498,9 +552,9 @@ public class TauP_XY extends TauP_AbstractTimeTool {
 
     public String getUsage() {
         return getStdUsage()
-                +"-x type            -- x axis value, one of "+String.join(", ", getAxisTypes())+".\n"
+                +"-x type            -- x axis value, default is degree, one of "+String.join(", ", getAxisTypes())+".\n"
                 +"-xlog              -- x axis is log.\n"
-                +"-y type            -- y axis value, same items as -x.\n"
+                +"-y type            -- y axis value, default is time, same items as -x.\n"
                 +"-ylog              -- y axis is log.\n"
                 +"--gmt              -- outputs curves as a complete GMT script.\n"
                 +"--svg              -- outputs curves as a SVG image.\n"
@@ -515,6 +569,29 @@ public class TauP_XY extends TauP_AbstractTimeTool {
     public String[] getAxisTypes() {
         return axisTypes;
     }
+
+    /**
+     * True if the axis type is distance-like.
+     *
+     * @param axisType
+     * @return
+     */
+    public boolean axisIsDistance(String axisType) {
+        return axisType.equalsIgnoreCase("degree")
+                || axisType.equalsIgnoreCase("degree_180")
+                || axisType.equalsIgnoreCase("radian")
+                || axisType.equalsIgnoreCase("radian_pi");
+    }
+
+    /**
+     * True if the axis type is time.
+     * @param axisType
+     * @return
+     */
+    public boolean axisIsTime(String axisType) {
+        return axisType.equalsIgnoreCase("time");
+    }
+
     @Override
     public void validateArguments() throws TauModelException {
 
@@ -568,11 +645,86 @@ public class TauP_XY extends TauP_AbstractTimeTool {
         this.yAxisLog = yAxisLog;
     }
 
+    public boolean isReduceTime() {
+        return reduceTime;
+    }
+
+    public void setReduceTime(boolean reduceTime) {
+        this.reduceTime = reduceTime;
+    }
+
+    /**
+     * @return reducing velocity in degrees/second. The internal usage is
+     *          radians/second.
+     */
+    public double getReduceVelDeg() {
+        return 180.0 / Math.PI * reduceVel;
+    }
+
+    /**
+     * set the reducing velocity, in degrees/second. The internal representation
+     * is radians/second.
+     */
+    public void setReduceVelDeg(double reduceVel) {
+        if(reduceVel > 0.0) {
+            redVelString = reduceVel+" deg/s";
+            this.reduceVel = Math.PI / 180.0 * reduceVel;
+        }
+    }
+
+    /**
+     * @return reducing velocity in kilometers/second. The internal usage is
+     *          radians/second.
+     */
+    public double getReduceVelKm() {
+        return reduceVel * tMod.getRadiusOfEarth();
+    }
+
+    public double reduceVelForAxis(String axisType) {
+        if (axisType.equalsIgnoreCase("degree")) {
+            return getReduceVelDeg();
+        } else if (axisType.equalsIgnoreCase("kilometer")) {
+            return getReduceVelKm();
+        } else if (axisType.equalsIgnoreCase("radian")) {
+            return getReduceVelDeg()*Math.PI/180;
+        } else {
+            throw new IllegalArgumentException("axis type not distance-like: "+axisType);
+        }
+    }
+
+    /**
+     * set the reducing velocity, in kilometers/second. The internal
+     * representation is radians/second.
+     */
+    public void setReduceVelKm(double reduceVel) {
+        redVelString = reduceVel+" km/s";
+        if(reduceVel > 0.0) {
+            if(tMod != null) {
+                this.reduceVel = reduceVel / tMod.getRadiusOfEarth();
+            } else {
+                this.reduceVel = reduceVel / 6371.0;
+            }
+        } else {
+            throw new IllegalArgumentException("Reducing velocity must be positive: "+reduceVel);
+        }
+    }
+
     protected String xAxisType = "degree";
     protected String yAxisType = "time";
 
     protected boolean xAxisLog = false;
     protected boolean yAxisLog = false;
+
+    /** should the output times use a reducing velocity? */
+    protected boolean reduceTime = false;
+
+    /**
+     * the reducing velocity to use if reduceTime == true, in units of
+     * radians/second .
+     */
+    protected double reduceVel = .125 * Math.PI / 180;
+
+    protected String redVelString = ".125 deg/s";
 
     protected double[] xAxisMinMax = new double[0];
     protected double[] yAxisMinMax = new double[0];
