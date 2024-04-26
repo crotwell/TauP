4fa5163d0ac28e196c672bf17544a0860954d7bd
Philip Crotwell <crotwell@seis.sc.edu>
2024-04-26 13:26:58 -0400

    temp disable seisplotjs/minseed3

--- src/webserver/resources/edu/sc/seis/webtaup/html/taup.js
+++ src/webserver/resources/edu/sc/seis/webtaup/html/taup.js
@@ -1,5 +1,6 @@
 import { startAnimation } from './wavefront_animation.js';
-import * as sp from './seisplotjs_3.1.4-SNAPSHOT_standalone.mjs';
+
+//import * as sp from './seisplotjs_3.1.4-SNAPSHOT_standalone.mjs';
 
 /**
  * Set up form listeners and other initialization items.
@@ -116,28 +117,30 @@ export async function display_results(taup_url) {
         }
       });
     } else if (format === "ms3") {
-      return response.arrayBuffer().then(rawBuffer => {
-        const dataRecords = sp.mseed3.parseMSeed3Records(rawBuffer);
-        return dataRecords;
-      }).then(dataRecords => {
-        return sp.mseed3.seismogramPerChannel(dataRecords);
-      }).then(seisList => {
-        const data = seisList[0].y;
-        let seisConfig = new sp.seismographconfig.SeismographConfig();
-        seisConfig.isRelativeTime = true;
-        seisConfig.amplitudeMode = sp.scale.AMPLITUDE_MODE.Raw;
-        const sddList = seisList.map(seis => sp.seismogram.SeismogramDisplayData.fromSeismogram(seis));
-        const seismograph = new sp.organizeddisplay.OrganizedDisplay(sddList, seisConfig);
-        //const seismograph = new sp.seismograph.Seismograph(sddList, seisConfig);
-        seismograph.addStyle(`
-          sp-seismograph {
-            height: 400px;
-          }
-        `);
-        container_el.appendChild(seismograph);
-        seismograph.draw();
+      console.log("miniseed3 format disabled"); 
 
-      });
+      // return response.arrayBuffer().then(rawBuffer => {
+      //   const dataRecords = sp.mseed3.parseMSeed3Records(rawBuffer);
+      //   return dataRecords;
+      // }).then(dataRecords => {
+      //   return sp.mseed3.seismogramPerChannel(dataRecords);
+      // }).then(seisList => {
+      //   const data = seisList[0].y;
+      //   let seisConfig = new sp.seismographconfig.SeismographConfig();
+      //   seisConfig.isRelativeTime = true;
+      //   seisConfig.amplitudeMode = sp.scale.AMPLITUDE_MODE.Raw;
+      //   const sddList = seisList.map(seis => sp.seismogram.SeismogramDisplayData.fromSeismogram(seis));
+      //   const seismograph = new sp.organizeddisplay.OrganizedDisplay(sddList, seisConfig);
+      //   //const seismograph = new sp.seismograph.Seismograph(sddList, seisConfig);
+      //   seismograph.addStyle(`
+      //     sp-seismograph {
+      //       height: 400px;
+      //     }
+      //   `);
+      //   container_el.appendChild(seismograph);
+      //   seismograph.draw();
+      //
+      // });
     }
   });
 }
