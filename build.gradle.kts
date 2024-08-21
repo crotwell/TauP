60046757116ea0a3c4f46e88d4b06686f6a22af4
Philip Crotwell <crotwell@seis.sc.edu>
2024-08-21 15:34:11 -0400

    dep update

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,13 +95,13 @@ dependencies {
     //implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 
-    runtimeOnly("org.slf4j:slf4j-reload4j:2.0.5")
+    runtimeOnly("org.slf4j:slf4j-reload4j:2.0.6")
 
     webserverImplementation("io.undertow:undertow-core:2.3.15.Final")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
