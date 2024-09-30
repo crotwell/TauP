9de776911a4cc75acc49068cefee6253ec3d4d4b
Philip Crotwell <crotwell@seis.sc.edu>
2024-09-30 10:25:52 -0400

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -125,11 +125,11 @@ dependencies {
 
     runtimeOnly("org.slf4j:slf4j-reload4j:2.0.6")
 
-    webserverImplementation("io.undertow:undertow-core:2.3.15.Final")
+    webserverImplementation("io.undertow:undertow-core:2.3.17.Final")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.1")
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
