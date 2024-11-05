aa1484b7e38bfd615b652a21abacbe65874a49e4
Philip Crotwell <crotwell@seis.sc.edu>
2024-11-05 09:14:59 -0500

    dep updates, new seisFile

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,9 +111,9 @@ val webserverImplementation by configurations.getting {
 
 dependencies {
     implementation("org.json:json:20240303")
-    implementation("edu.sc.seis:seisFile:2.2.1-SNAPSHOT") {
+    //implementation("edu.sc.seis:seisFile:2.2.2-SNAPSHO") {
     // or
-    //implementation("edu.sc.seis:seisFile:2.2.0") {
+    implementation("edu.sc.seis:seisFile:2.2.1") {
       // we need seisFile for sac/mseed3 output, but not all the other functionality
       exclude(group = "org.apache.httpcomponents", module = "httpclient")
     }
@@ -125,11 +125,11 @@ dependencies {
 
     runtimeOnly("org.slf4j:slf4j-reload4j:2.0.6")
 
-    webserverImplementation("io.undertow:undertow-core:2.3.17.Final")
+    webserverImplementation("io.undertow:undertow-core:2.3.18.Final")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
