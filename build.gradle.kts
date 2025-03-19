def231bd099b4f5b271269261aeb8719ed6761c5
Philip Crotwell <crotwell@seis.sc.edu>
2025-03-19 12:00:10 -0400

    dep upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,8 +128,8 @@ dependencies {
     webserverImplementation("io.undertow:undertow-core:2.3.18.Final")
 
         // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.1")
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
