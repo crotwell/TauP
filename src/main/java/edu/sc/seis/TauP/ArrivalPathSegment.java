29a4f2d366189db74a409054143916771b3da818
Philip Crotwell <crotwell@seis.sc.edu>
2025-05-08 10:05:01 -0400

    adjust pierce points via shifty if end distance isn't same as arrival

--- src/main/java/edu/sc/seis/TauP/ArrivalPathSegment.java
+++ src/main/java/edu/sc/seis/TauP/ArrivalPathSegment.java
@@ -20,6 +20,21 @@ public class ArrivalPathSegment extends AbstractPathSegment {
 
 
     /**
+     * Adjust pierce points so the end point lines up.
+     * Because we are shooting a ray parameter through the model, and that ray parameter came from an
+     * interpolation, it can happen for long paths that the output path doesn't quite end at the requested
+     * distance. We do a simple scaling of all pierce distances so it hits the output.
+     * @param inPath input path
+     * @param arrival arrival for path
+     * @return adjusted path
+     */
+    public static List<TimeDist> adjustPierce(List<TimeDist> inPath, Arrival arrival) {
+        double shifty = calcShiftyForDist(inPath.get(0), inPath.get(inPath.size()-1), arrival);
+        List<TimeDist> out = adjustPathForShifty(inPath, shifty);
+        return out;
+    }
+
+    /**
      * Adjust path so the end point lines up.
      * Because we are shooting a ray parameter through the model, and that ray parameter came from an
      * interpolation, it can happen for long paths that the output path doesn't quite end at the requested
@@ -30,15 +45,15 @@ public class ArrivalPathSegment extends AbstractPathSegment {
      */
     public static List<ArrivalPathSegment> adjustPath(List<ArrivalPathSegment> inPath, Arrival arrival) {
         double distRadian = arrival.getDist();
+        if (inPath.size()==0) {
+            throw new RuntimeException("can adjust empty path for phase "+arrival.getName());
+        }
         // start might not be zero is part of scattered phase
         TimeDist firstPoint = inPath.get(0).getPathStart();
         ArrivalPathSegment lastSeg = inPath.get(inPath.size()-1);
         double finalPathDist = lastSeg.getPathEnd().getDistRadian()- firstPoint.getDistRadian();
-        if (!inPath.isEmpty() && distRadian != 0 && finalPathDist != 0) {
-            double shifty = distRadian/finalPathDist;
-            if (arrival.isLongWayAround()) {
-                shifty *= -1;
-            }
+        if (!inPath.isEmpty()) {
+            double shifty = calcShiftyForDist(firstPoint, lastSeg.getPathEnd(), arrival);
             if (Math.abs(1.0-shifty) > .02 && Math.abs(1+shifty) > 1e-5 ) {
                 // don't flag shifty that just reflects, ie -1
                 Alert.warning("Path error is greater than 2%, correction may cause errors. "+shifty);
@@ -48,7 +63,7 @@ public class ArrivalPathSegment extends AbstractPathSegment {
             List<ArrivalPathSegment> out = new ArrayList<>();
             TimeDist prevEnd = inPath.get(0).prevEnd;
             for (ArrivalPathSegment seg : inPath) {
-                ArrivalPathSegment shiftySeg = new ArrivalPathSegment(seg.adjustPathForShifty(shifty),
+                ArrivalPathSegment shiftySeg = new ArrivalPathSegment(seg.adjustPathForShifty(seg.path, shifty),
                         seg.isPWave, seg.segmentName, prevEnd, seg.arrival,
                         seg.phaseSegment, seg.segmentIndex, seg.totalNumSegments);
                 prevEnd = shiftySeg.getPathEnd();
@@ -60,7 +75,20 @@ public class ArrivalPathSegment extends AbstractPathSegment {
         }
     }
 
-    public List<TimeDist> adjustPathForShifty(double shifty) {
+    public static double calcShiftyForDist(TimeDist firstPoint, TimeDist lastPoint, Arrival arrival) {
+        double distRadian = arrival.getDist();
+        double finalPathDist = lastPoint.getDistRadian()- firstPoint.getDistRadian();
+        double shifty = 0;
+        if (distRadian != 0 && finalPathDist != 0) {
+            shifty = distRadian / finalPathDist;
+            if (arrival.isLongWayAround()) {
+                shifty *= -1;
+            }
+        }
+        return shifty;
+    }
+
+    public static List<TimeDist> adjustPathForShifty(List<TimeDist> path, double shifty) {
         ArrayList<TimeDist> out = new ArrayList<>();
         for (TimeDist td : path) {
             out.add(new TimeDist(td.getP(),
