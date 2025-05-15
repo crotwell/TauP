285fe70bcaed23757dcb184daa442cfca196d239
Philip Crotwell <crotwell@seis.sc.edu>
2025-05-15 15:13:09 -0400

    update homebrew formula

--- installers/homebrew/taup.rb
+++ installers/homebrew/taup.rb
@@ -1,15 +1,13 @@
 class Taup < Formula
   desc "Flexible Seismic Travel-Time and Raypath Utilities"
   homepage "https://www.seis.sc.edu/TauP/"
-  url "https://www.seis.sc.edu/downloads/TauP/TauP-2.5.0.tgz"
-  sha256 "5412c96516dab825e77791ed00c28d5ddcb2f66b5d19dd4aad8cbb60f36f1b66"
+  url "https://zenodo.org/records/15116393/files/TauP-3.0.0.tar.gz?download=1"
+  sha256 "d7cc97b1eaf04e1e4861cb9698f8b7439ee47c806926e331972122f75666633f"
   license "LGPL-3.0-or-later"
-
-  bottle :unneeded
+  depends_on "openjdk"
 
   def install
-    rm_f Dir["bin/*.bat"]
-    rm_f Dir["bin/taup_*"]
+    rm Dir["bin/*.bat"]
     libexec.install %w[bin docs lib src]
     env = if Hardware::CPU.arm?
       Language::Java.overridable_java_home_env("11")
