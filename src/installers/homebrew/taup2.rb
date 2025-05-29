82690cfef3eb05192a48a7a486df5183c1341232
Philip Crotwell <crotwell@seis.sc.edu>
2025-05-29 14:09:08 -0400

    use zenodo for taup2 download, fix rm_f for audit

--- src/installers/homebrew/taup2.rb
+++ src/installers/homebrew/taup2.rb
@@ -1,14 +1,15 @@
 class Taup2 < Formula
   desc "Flexible Seismic Travel-Time and Raypath Utilities"
   homepage "https://www.seis.sc.edu/TauP/"
-  url "https://github.com/crotwell/TauP/releases/download/v2.6.1/TauP-2.6.1.tgz"
+  url "https://zenodo.org/records/10794858/files/TauP-2.6.1.tgz?download=1"
   sha256 "4041e3e9cbbbfde4196723b2e7e81f30800370056912b6be664a070526d99494"
   license "LGPL-3.0-or-later"
+  revision 3
   depends_on "openjdk@11"
 
   def install
-    rm_f Dir["bin/*.bat"]
-    rm_f Dir["bin/taup_*"]
+    rm Dir["bin/*.bat"]
+    rm Dir["bin/taup_*"]
     libexec.install %w[bin docs lib src]
     env = Language::Java.overridable_java_home_env
     (bin/"taup2").write_env_script libexec/"bin/taup", env
