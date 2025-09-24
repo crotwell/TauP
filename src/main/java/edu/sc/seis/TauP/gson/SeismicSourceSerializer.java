f526a194cbf2eb029db73eaba9edf2801f2510f5
Philip Crotwell <crotwell@seis.sc.edu>
2025-09-24 14:45:18 -0400

    serializer for SeismicSource

--- src/main/java/edu/sc/seis/TauP/gson/SeismicSourceSerializer.java
+++ src/main/java/edu/sc/seis/TauP/gson/SeismicSourceSerializer.java
@@ -6,19 +6,19 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import edu.sc.seis.TauP.FaultPlane;
 import edu.sc.seis.TauP.JSONLabels;
+import edu.sc.seis.TauP.SeismicSource;
 import edu.sc.seis.TauP.cmdline.args.SeismicSourceArgs;
 
 import java.lang.reflect.Type;
 
-public class SourceArgsSerializer implements JsonSerializer<SeismicSourceArgs> {
+public class SeismicSourceSerializer implements JsonSerializer<SeismicSource> {
 
     @Override
-    public JsonElement serialize(SeismicSourceArgs src, Type typeOfSrc, JsonSerializationContext context) {
+    public JsonElement serialize(SeismicSource src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject json = new JsonObject();
         json.addProperty(JSONLabels.MW, src.getMw());
-        json.addProperty(JSONLabels.ATTEN_FREQ, src.getAttenuationFrequency());
-        if (src.hasStrikeDipRake()) {
-            FaultPlane faultPlane = src.getFaultPlane();
+        if (src.hasNodalPlane()) {
+            FaultPlane faultPlane = src.getNodalPlane1();
             JsonObject jsonSDR = new JsonObject();
             json.add(JSONLabels.FAULT, jsonSDR);
             jsonSDR.addProperty(JSONLabels.STRIKE, faultPlane.getStrike());
@@ -26,6 +26,8 @@ public class SourceArgsSerializer implements JsonSerializer<SeismicSourceArgs> {
             jsonSDR.addProperty(JSONLabels.RAKE, faultPlane.getRake());
 
         }
+        json.addProperty(JSONLabels.ATTEN_FREQ, src.getAttenuationFrequency());
+        json.addProperty(JSONLabels.ATTEN_NUM_FREQ, src.getNumFrequencies());
         return json;
     }
 }
