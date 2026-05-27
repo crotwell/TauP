878e78c34c48971a9666c0c8e102cfdf9b8c0914
Philip Crotwell <crotwell@seis.sc.edu>
2026-05-27 15:18:50 -0400

    very simple example from taup_python, again

--- src/doc/sphinx/source/programming/example_very_simple.py
+++ src/doc/sphinx/source/programming/example_very_simple.py
@@ -6,10 +6,10 @@ with taup.TauPServer() as taupserver:
     params = taup.TimeQuery()
     params.phase(["P", "S"])
     params.degree(35)
-    taupResult = params.calc(taupserver)
-    if len(taupResult.arrivals) == 0:
+    timeResult = params.calc(taupserver)
+    if len(timeResult.arrivals) == 0:
         print(f"No arrivals...")
     else:
         print("Phase  Depth   Dist   Time")
-        for a in taupResult.arrivals:
+        for a in timeResult.arrivals:
             print(f"{a.phase}      {a.sourcedepth}     {a.distdeg}   {a.time}")
