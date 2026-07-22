d010f6b5a0b5e04f3df750877502b9a928116d27
Philip Crotwell <crotwell@seis.sc.edu>
2026-07-22 15:13:50 -0400

    xy serialize simpler, due to xyplot title simplify

--- src/main/java/edu/sc/seis/TauP/gson/XYPlotOutputSerializer.java
+++ src/main/java/edu/sc/seis/TauP/gson/XYPlotOutputSerializer.java
@@ -8,19 +8,7 @@ import java.lang.reflect.Type;
 
 public class XYPlotOutputSerializer implements JsonSerializer<XYPlotOutput> {
     public JsonElement serialize(XYPlotOutput xy, Type typeOfSrc, JsonSerializationContext context) {
-        JsonObject out;
-        ModelArgs modelArgs = xy.getModelArgs();
-        if (modelArgs != null ) {
-            try {
-                AbstractPhaseResult phaseResult = new AbstractPhaseResult(modelArgs.getModelName(), modelArgs.getSourceDepths(),
-                        modelArgs.getReceiverDepths(), xy.getPhaseNames(), modelArgs.getScatterer(), false, null);
-                out = (JsonObject) context.serialize(phaseResult);
-            } catch (TauModelException e) {
-                throw new RuntimeException("Unable to load model, should not happen: "+modelArgs.getModelName());
-            }
-        } else {
-            out = new JsonObject();
-        }
+        JsonObject out = new JsonObject();
         JsonArray phaseCurves = new JsonArray();
         for (XYPlottingData plotItem : xy.getXYPlots()) {
             phaseCurves.add(context.serialize(plotItem));
